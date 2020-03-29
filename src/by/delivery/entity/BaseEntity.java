91cf84ab663711cbdccf5acab71c225c5354e5a3
Valery Karpov <karpoff19@mail.ru>
2020-03-29 19:59:10 +0300

    add

---
+++ src/by/delivery/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package by.delivery.entity;
+
+public abstract class BaseEntity {
+
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/by/delivery/entity/User.java
@@ -0,0 +1,142 @@
+package by.delivery.entity;
+
+import java.util.Objects;
+
+public class User extends BaseEntity {
+
+    private String phoneNumber;
+    private String name;
+    private String surname;
+    private String address;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(phoneNumber, user.phoneNumber) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(address, user.address) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber, name, surname, address, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "phoneNumber=" + phoneNumber +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private User newUser;
+
+        public Builder() {
+            newUser = new User();
+        }
+
+        public Builder setId(Long id) {
+            newUser.setId(id);
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            newUser.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            newUser.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            newUser.surname = surname;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            newUser.address = address;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            newUser.email = email;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            newUser.password = password;
+            return this;
+        }
+
+        public User build() {
+            return newUser;
+        }
+    }
+}
